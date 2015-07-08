4d8c2c359772db9c8b9438a669d6f80a97bb8021
kurudrive <kurudrive@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-08 10:01:12 +0000

    0.1.4.1
    git-svn-id: https://plugins.svn.wordpress.org/vk-all-in-one-expansion-unit/trunk@1194661 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vkExUnit.php
+++ vkExUnit.php
@@ -3,7 +3,7 @@
 Plugin Name: VK All in One Expansion Unit
 Plugin URI: https://github.com/kurudrive/VK-All-in-one-Expansion-Unit
 Description: This plug-in is an integrated plug-in with a variety of features that make it powerful your web site. Example Facebook Page Plugin,Social Bookmarks,Print OG Tags,Print Twitter Card Tags,Print Google Analytics tag,New post widget,Insert Related Posts and more!
-Version: 0.1.4.0
+Version: 0.1.4.1
 Author: Vektor,Inc.
 Author URI: http://vektor-inc.co.jp
 License: GPL2
@@ -119,9 +119,9 @@ add_action('wp_enqueue_scripts','vkExUnit_print_css');
 function vkExUnit_print_css(){
 	$options = vkExUnit_get_common_options();
 	if ( isset($options['active_bootstrap']) && $options['active_bootstrap'] ) {
-		wp_enqueue_style('vkExUnit_common_style', plugins_url('', __FILE__).'/css/style_in_bs.css', array(), '20150525', 'all');
+		wp_enqueue_style('vkExUnit_common_style', plugins_url('', __FILE__).'/css/style_in_bs.css', array(), '20150708', 'all');
 	} else {
-		wp_enqueue_style('vkExUnit_common_style', plugins_url('', __FILE__).'/css/style.css', array(), '20150525', 'all');	
+		wp_enqueue_style('vkExUnit_common_style', plugins_url('', __FILE__).'/css/style.css', array(), '20150708', 'all');	
 	}
 }
 /*-------------------------------------------*/
@@ -131,9 +131,9 @@ add_action('wp_head','vkExUnit_addJs');
 function vkExUnit_addJs(){
 	$options = vkExUnit_get_common_options();
 	if ( isset($options['active_bootstrap']) && $options['active_bootstrap'] ) {
-	wp_register_script( 'vkExUnit_master-js' , plugins_url('', __FILE__).'/js/all_in_bs.min.js', array('jquery'), '20150628' );
+	wp_register_script( 'vkExUnit_master-js' , plugins_url('', __FILE__).'/js/all_in_bs.min.js', array('jquery'), '20150708' );
 	} else {
-		wp_register_script( 'vkExUnit_master-js' , plugins_url('', __FILE__).'/js/all.min.js', array('jquery'), '20150628' );
+		wp_register_script( 'vkExUnit_master-js' , plugins_url('', __FILE__).'/js/all.min.js', array('jquery'), '20150708' );
 	}
 	wp_enqueue_script( 'vkExUnit_master-js' );
 }
